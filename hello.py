939cf826b244c9952d7426a1db736ca2d90cafc9
Wei Liu <liuweiself@126.com>
2016-01-08 02:34:16 +0800

    static

--- hello.py
+++ hello.py
@@ -7,14 +7,26 @@ from flask import make_response
 from flask import redirect
 from flask.ext.script import Manager
 from flask import render_template
+from flask.ext.bootstrap import Bootstrap
+
+
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
+#指定自定义错误页面
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),400
 
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'),500
 
-
-
+@app.route('/base')
+def basepage():
+	return render_template('base.html')
 
 @app.route('/')
 def index():
