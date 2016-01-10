71ee198c2fbec54447b414b232215355a5f80b46
Wei Liu <liuweiself@126.com>
2016-01-10 23:53:08 +0800

    a

--- hello.py
+++ hello.py
@@ -3,28 +3,18 @@
 
 from flask import Flask
 from flask import make_response
+from flask import redirect
 from flask.ext.script import Manager
-from flask import render_template,session,redirect,url_for,flash
+from flask import render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import StringField,SubmitField,PasswordField
 from wtforms.validators import Required
-from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'hard to guess string'
-
-#配置数据库
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/test'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
-db = SQLAlchemy(app)
-
-
-
 # manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment()
@@ -33,6 +23,13 @@ class NameForm(Form):
 	name = StringField(u'用户名',validators = [Required()])
 	password = PasswordField(u'密码',validators = [Required()])
 	submit = SubmitField(u'登录')
+
+
+
+
+
+
+#nick/....
 
 #指定自定义错误页面
 @app.errorhandler(404)
@@ -44,26 +41,25 @@ def internal_server_error(e):
 	return render_template('500.html'),500
 
 ####
-@app.route('/',methods= ['GET','POST'])
+@app.route('/index')
 def index():
 	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name = session['name']
-		if old_name is not None and old_name != form.name.data:
-			flash(u'looks your changed your name')#仅调用flash（）并不能把消息显示出来。需要在模板中渲染Flash消息
-		session['name'] = form.name.data #相当于NSUserDefaluts.像dict一样使用它
-		return redirect(url_for('index'))#redirect()重定向  url_for()传入端点名。即响应视图函数的函数名
-	return render_template('index.html',form=form,name=session['name'])
+		name = form.name.data
+		form.name.data= ''
+	return render_template('index.html',form=form,name=name)
 
 
 @app.route('/base')
 def basepage():
 	return render_template('base.html')
 
+
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html',name=name)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
