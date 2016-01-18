39c3f5a24414f22000fe2e66d9a65c9bb662c267
waynezxcv <liuweiself@126.com>
2016-01-18 21:22:38 +0800

    xx

--- myapp/manage.py
+++ myapp/manage.py
@@ -39,6 +39,8 @@ manager.add_command("shell",Shell(make_context = make_shell_context))
 manager.add_command('db',MigrateCommand)
 
 
+server = Server(host="0.0.0.0", port=9000)
+
 
 if __name__ == '__main__':
 	manager.run()
