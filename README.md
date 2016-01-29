67fbc0c1e8b07e8d6c891b7dce7cbdc1a0fed8e0
waynezxcv <liuweiself@126.com>
2016-01-29 11:42:20 +0800

    release

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Waynezxcv
-我的个人博客。使用Flask框架的后台。Bootstrap模板框架实现前端。
+使用Flask构建的网站后台。
+Nginx + uWSGI + Flask + MySQL+ Linux
--- myapp/config.py
+++ myapp/config.py
@@ -6,9 +6,9 @@ import sys
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is waynezxcvs secretkey'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or ''
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-	WAYNEZXCV_ADMIN = os.environ.get('WAYNEZXCV_ADMIN')
+	WAYNEZXCV_ADMIN = os.environ.get('')
 	
 	@staticmethod
 	def init_app(app):
@@ -17,16 +17,16 @@ class Config(object):
 #开发环境
 class DevelopmentConfig(Config):
 	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'mysql+mysqlconnector://root:password@localhost:3306/waynezxcv'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or ''
 
 #测试环境
 class TestingConfig(Config):
 	TESTING = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'mysql+mysqlconnector://root:password@localhost:3306/waynezxcv'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or ''
 
 #生产环境
 class ProductionConfig(Config):
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://root:password@localhost:3306/waynezxcv'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or ''
 
 
 config = {
